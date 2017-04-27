94dc55a801c21bc7ac7d5425f5961d993b20c312
sgilley <sgilley@microsoft.com>
2017-04-27 09:57:24 -0500

    fix typo

--- RSparkCluster/Copy_Dev2Prod.R
+++ RSparkCluster/Copy_Dev2Prod.R
@@ -14,27 +14,27 @@
 
 # Define the function to copy model related objects from dev/model folder to prod/model folder
 CopyFromDev2Prod <- function(DevModelDir,
-                             ProdModellDir){
+                             ProdModelDir){
   
   # check the prod folder stores model related objexts if exists
   # if exists, remove and create a new one
   # if not exists, create a new one
-  if(dir.exists(ProdModellDir)){
-    system(paste("rm -rf ",ProdModellDir, sep="")) # remove the directory if exists
-    system(paste("mkdir -p -m 777 ", ProdModellDir, sep="")) # create a new directory
+  if(dir.exists(ProdModelDir)){
+    system(paste("rm -rf ",ProdModelDir, sep="")) # remove the directory if exists
+    system(paste("mkdir -p -m 777 ", ProdModelDir, sep="")) # create a new directory
   } else {
-    system(paste("mkdir -p -m 777 ", ProdModellDir, sep="")) # make new directory if doesn't exist
+    system(paste("mkdir -p -m 777 ", ProdModelDir, sep="")) # make new directory if doesn't exist
   }
   
   # copy files from dev folder to prod folder
-  system(paste("cp ", DevModelDir, "*.rds ", ProdModellDir, sep = ""))
+  system(paste("cp ", DevModelDir, "*.rds ", ProdModelDir, sep = ""))
 }
 
 
 # Specify the source folder and destination folder
 DevModelDir <- paste("/var/RevoShare/", Sys.info()[["user"]], "/Campaign/dev/model/", sep="" )
-ProdModellDir <- paste("/var/RevoShare/", Sys.info()[["user"]], "/Campaign/prod/model/", sep="" )
+ProdModelDir <- paste("/var/RevoShare/", Sys.info()[["user"]], "/Campaign/prod/model/", sep="" )
 
 # Invoke the function
 CopyFromDev2Prod (DevModelDir = DevModelDir,
-                  ProdModellDir = ProdModellDir)
+                  ProdModelDir = ProdModelDir)

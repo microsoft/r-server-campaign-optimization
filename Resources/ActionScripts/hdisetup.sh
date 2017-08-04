69166edad418b623212678f07fae31f5c4e08f8a
sgilley <sgilley@microsoft.com>
2017-08-04 10:42:04 -0500

    debug

--- Resources/ActionScripts/hdisetup.sh
+++ Resources/ActionScripts/hdisetup.sh
@@ -12,8 +12,8 @@ rm -rf campaign
 sed -i "s/XXYOURPW/$2/g" /home/$1/*.R
 
 # Configure edge node as one-box setup for R Server Operationalization
-/usr/local/bin/dotnet /usr/lib64/microsoft-r/rserver/o16n/9.1.0/Microsoft.RServer.Utils.AdminUtil/Microsoft.RServer.Utils.AdminUtil.dll -silentoneboxinstall "$2"
+#/usr/local/bin/dotnet /usr/lib64/microsoft-r/rserver/o16n/9.1.0/Microsoft.RServer.Utils.AdminUtil/Microsoft.RServer.Utils.AdminUtil.dll -silentoneboxinstall "$2"
 
 # turn off telemetry 
-sed -i 's/options(mds.telemetry=1)/options(mds.telemetry=0)/g' /usr/lib64/microsoft-r/3.3/lib64/R/etc/Rprofile.site
-sed -i 's/options(mds.logging=1)/options(mds.logging=0)/g' /usr/lib64/microsoft-r/3.3/lib64/R/etc/Rprofile.site
+#sed -i 's/options(mds.telemetry=1)/options(mds.telemetry=0)/g' /usr/lib64/microsoft-r/3.3/lib64/R/etc/Rprofile.site
+#sed -i 's/options(mds.logging=1)/options(mds.logging=0)/g' /usr/lib64/microsoft-r/3.3/lib64/R/etc/Rprofile.site

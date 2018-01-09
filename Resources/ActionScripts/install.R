d34908b1c3015e0b26b289beca4ff51b1e375389
v-robwhi <v-robwhi@microsoft.com>
2018-01-09 12:06:55 -0800

    Updating to New DSVM and Setup Script  (#11)
    * checking in
    
    * used correct branch
    
    * fixed jupyrt notebook
    
    * updated set up for Filestream access
    
    * Updated Setup and SQL Configuration to work with more solutions
    
    * updated positional parameters in ConfigureSQL.ps1
    
    * fixed setup
    
    * refixed setup
    
    * Testing to see if Enabling file stream works
    
    * testing File Stream Access
    
    * testing file stream
    
    * done testing filestream
    
    * changed to securstring password
    
    * fixed
    
    * changed to master branch on git

--- Resources/ActionScripts/install.R
+++ Resources/ActionScripts/install.R
@@ -1,15 +1,16 @@
 # installing packages for SQL Server 
 
-install.packages("RODBC", lib="C:/Program Files/Microsoft SQL Server/MSSQL13.MSSQLSERVER/R_SERVICES/library")
-install.packages("ggplot2", lib="C:/Program Files/Microsoft SQL Server/MSSQL13.MSSQLSERVER/R_SERVICES/library")
-install.packages("reshape", lib="C:/Program Files/Microsoft SQL Server/MSSQL13.MSSQLSERVER/R_SERVICES/library")
-install.packages("scales", lib="C:/Program Files/Microsoft SQL Server/MSSQL13.MSSQLSERVER/R_SERVICES/library")
+install.packages("RODBC", lib="C:/Program Files/Microsoft SQL Server/MSSQL14.MSSQLSERVER/R_SERVICES/library")
+install.packages("ggplot2", lib="C:/Program Files/Microsoft SQL Server/MSSQL14.MSSQLSERVER/R_SERVICES/library")
+install.packages("reshape", lib="C:/Program Files/Microsoft SQL Server/MSSQL14.MSSQLSERVER/R_SERVICES/library")
+install.packages("scales", lib="C:/Program Files/Microsoft SQL Server/MSSQL14.MSSQLSERVER/R_SERVICES/library")
+
+install.packages("RODBC", lib="C:/Program Files/Microsoft/ML Server/R_SERVER/library")
+install.packages("ggplot2", lib="C:/Program Files/Microsoft/ML Server/R_SERVER/library")
+install.packages("reshape", lib="C:/Program Files/Microsoft/ML Server/R_SERVER/library")
+install.packages("scales", lib="C:/Program Files/Microsoft/ML Server/R_SERVER/library")
 
 
-install.packages("RODBC", lib="C:/Program Files/Microsoft/R Server/R_SERVER/library")
-install.packages("ggplot2", lib="C:/Program Files/Microsoft/R Server/R_SERVER/library")
-install.packages("reshape", lib="C:/Program Files/Microsoft/R Server/R_SERVER/library")
-install.packages("scales", lib="C:/Program Files/Microsoft/R Server/R_SERVER/library")
 
 
 print("Done installing packages")

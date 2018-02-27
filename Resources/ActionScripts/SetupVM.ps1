62b2a5fe6d9f5dd7f00f600a03e1cb217e52786d
Bob White <v-robwhi@microsoft.com>
2018-02-27 09:04:19 -0800

    Fixed Typos in comments

--- Resources/ActionScripts/SetupVM.ps1
+++ Resources/ActionScripts/SetupVM.ps1
@@ -17,7 +17,6 @@ param(
 [ValidateNotNullOrEmpty()] 
 [string]$Prompt
 )
-$startTime = Get-Date
 
 
 
@@ -63,12 +62,13 @@ $SolutionData = $SolutionPath + "\Data\"
 Write-Host "Installing SQLServer Power Shell Module or Updating to latest "
 
 
-if (Get-Module -ListAvailable -Name SQLServer) {Update-Module -Name "SQLServer" -MaximumVersion 21.0.17199}
+if (Get-Module -ListAvailable -Name SQLServer) 
+    {Update-Module -Name "SQLServer" -MaximumVersion 21.0.17199}
 Else 
-{Install-Module -Name SqlServer -RequiredVersion 21.0.17199 -Scope AllUsers -AllowClobber -Force}
+    {Install-Module -Name SqlServer -RequiredVersion 21.0.17199 -Scope AllUsers -AllowClobber -Force}
 
 #Set-PSRepository -Name PSGallery -InstallationPolicy Untrusted
-Import-Module -Name SqlServer -MaximumVersion 21.0.17199 -Force
+    Import-Module -Name SqlServer -MaximumVersion 21.0.17199 -Force
 
 
 

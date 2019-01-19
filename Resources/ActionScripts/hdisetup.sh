3b9d6b34cdb53b5181f5d354876ee3579686fe6d
Jeroen ter Heerdt <jterh@microsoft.com>
2019-01-18 17:20:57 -0800

    Updating hdisetup.sh for ML Server 9.3

--- Resources/ActionScripts/hdisetup.sh
+++ Resources/ActionScripts/hdisetup.sh
@@ -2,7 +2,7 @@
 
 # This script used to setup an HDInsight Cluster deployed from Cortana Analytics Gallery
 # WARNING: This script is only meant to be run from the solution template deployment process.
-
+# This should be run on the edge node of the cluster!
 # put R code in users home directory
 # XXX SWITCH to master branch when ready to publish!! XXX
 git clone  --branch master --single-branch  https://github.com/Microsoft/r-server-campaign-optimization.git  campaign
@@ -12,8 +12,11 @@ rm -rf campaign
 sed -i "s/XXYOURPW/$2/g" /home/$1/*.R
 
 # Configure edge node as one-box setup for R Server Operationalization
-sudo /usr/local/bin/dotnet /usr/lib64/microsoft-r/rserver/o16n/9.1.0/Microsoft.RServer.Utils.AdminUtil/Microsoft.RServer.Utils.AdminUtil.dll -silentoneboxinstall "$2"
+az extension add --source /opt/microsoft/mlserver/9.3.0/o16n/azure_ml_admin_cli-0.0.1-py2.py3-none-any.whl --yes
+az ml admin node setup --onebox --admin-password $2 --confirm-password $2
 
-# turn off telemetry 
-sed -i 's/options(mds.telemetry=1)/options(mds.telemetry=0)/g' /usr/lib64/microsoft-r/3.3/lib64/R/etc/Rprofile.site
-sed -i 's/options(mds.logging=1)/options(mds.logging=0)/g' /usr/lib64/microsoft-r/3.3/lib64/R/etc/Rprofile.site
+#run step0_data_generation.R
+Rscript step0_data_generation.R
+
+#run campaign_deployment.R
+Rscript campaign_deployment.R
